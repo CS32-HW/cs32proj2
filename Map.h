0f9feb99c95364ebdfaac0ffad4c278f60136f2b
Ethan Cheng <ethan@nijika.org>
2024-01-25 11:22:45 -0800

    inline getPair function

--- Map.h
+++ Map.h
@@ -32,9 +32,21 @@ class Map
     Pair *head, *tail;
     int map_size;
 
-    Pair* getPair(const KeyType& key) const;
+    Pair* getPair(const KeyType& key) const
       // returns a pointer to the Pair containing the key
       // returns nullptr if not found
+    {
+        Pair* p = head;
+
+        while (p != nullptr) {
+            if (p->key == key)
+                break;
+            p = p->next;
+        }
+
+        return p;
+    }
+
 };
 
 bool merge(const Map& m1, const Map& m2, Map& result);
